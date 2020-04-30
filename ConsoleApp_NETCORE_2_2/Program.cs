fa38599196e9fee430c7f216687747c07e8c3e15
jean-pierre gervasoni <jean-pierre.gervasoni@sydev.com>
2020-04-30 14:04:20 +0200

    Adding netcore2.2 target

---
+++ ConsoleApp_NETCORE_2_2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp_NETCORE_2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t = new NugetDemo.Class1();
+
+            Console.WriteLine(t.Platform);
+        }
+    }
+}
